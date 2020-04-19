78b01c8433cae37fceb84ca12f648c738b2c0d23
INT31302 <tkdwo287@gmail.com>
2020-04-20 04:02:07 +0900

    init

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python dog.py
---
+++ dog.py
@@ -0,0 +1,32 @@
+import discord
+import datetime
+client = discord.Client()
+TOKEN = "NzAxNDYyODg4NTg1NDk0NTc5.Xpx9Nw.UppwtjYFauget256wM1IcDP28e8"
+
+
+@client.event
+async def on_ready():
+    print(str(client.user.id)+" is ready")
+    game = discord.Game("테스트")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("/안녕"):
+        await message.channel.send("할말")
+    if message.content.startswith("/테스트"):
+        await message.channel.send("메롱")
+
+
+@client.event
+async def on_member_join(member):
+    try:
+        await member.create_dm()
+        await member.dm_channel.send("댕댕이애호가에 오신 것을 환영합니다! 공지사항 게시판에서 공지사항을 먼저 읽어주세요. 같이 즐겁게 로아합시다^^")
+    except:
+        print("error")
+    role = discord.utils.get(member.guild.roles, name="🔰길드원")
+    await member.add_roles(role)
+
+client.run(TOKEN)

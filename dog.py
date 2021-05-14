83988916ca4a887e56981575404361b6358126c4
Kim SangJae <51100935+INT31302@users.noreply.github.com>
2021-05-15 00:12:24 +0900

    Update dog.py

--- dog.py
+++ dog.py
@@ -112,7 +112,7 @@ def ladder(message=""):
     while(cnt != item_cnt):
         ran = random.randrange(0, people_cnt)
         if(check[ran] == False):
-            item_list[i].append(ran);
+            item_list[cnt].append(ran);
             cnt+=1;
             check[ran] = True;
     

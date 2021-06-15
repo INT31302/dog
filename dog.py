3aad9896cda7864d2bb28acf089f85aaf7216e4c
Kim SangJae <51100935+INT31302@users.noreply.github.com>
2021-06-16 01:33:04 +0900

    Update dog.py

--- dog.py
+++ dog.py
@@ -52,6 +52,7 @@ async def on_message(message):
 
 @client.event
 async def on_member_join(member):
+    print(member, '멤버왔음!')
     try:
         await member.create_dm()
         await member.dm_channel.send("댕댕이애호가에 오신 것을 환영합니다! 공지사항 게시판에서 공지사항을 먼저 읽어주세요. 같이 즐겁게 로아합시다^^ 서버 내 닉네임을 양식에 맡게 변경해주세요!")
